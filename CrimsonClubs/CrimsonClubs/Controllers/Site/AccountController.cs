6fc1e3c06fa7193bcec26547a04dca990aecbfe6
mlivingston <mdlivingston@crimson.ua.edu>
2018-03-30 16:15:04 -0500

    Added login page

---
+++ CrimsonClubs/CrimsonClubs/Controllers/Site/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CrimsonClubs.Controllers.Site
+{
+    public class AccountController : CCSiteController
+    {
+        [HttpGet, Route("account")]
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
