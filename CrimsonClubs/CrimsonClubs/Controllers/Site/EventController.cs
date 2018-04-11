7a39ba96e6b6d0261c08cb7ecd42ced4b3d47884
mlivingston <mdlivingston@crimson.ua.edu>
2018-04-11 16:09:56 -0500

    Added Full Event Viewing

---
+++ CrimsonClubs/CrimsonClubs/Controllers/Site/EventController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CrimsonClubs.Controllers.Site
+{
+    public class EventController : CCSiteController
+    {
+        [HttpGet, Route("event")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
