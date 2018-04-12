a6474cc935687971bffb56911596702f35e9a002
Maclane May <mdmay1@crimson.ua.edu>
2018-04-12 12:34:43 -0500

    - Added more information to ClubDto

--- CrimsonClubs/CrimsonClubs.Models/Dtos/ClubDto.cs
+++ CrimsonClubs/CrimsonClubs.Models/Dtos/ClubDto.cs
@@ -11,25 +11,31 @@ namespace CrimsonClubs.Models.Dtos
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public bool IsRequestToJoin { get; set; }
         public int? GroupId { get; set; }
         public string GroupName { get; set; }
         public int MemberCount { get; set; }
         public bool IsAccepted { get; set; }
+        public bool HasRequested { get; set; }
+        public bool IsAllowedToJoin { get; set; }
 
         public ClubDto()
         {
 
         }
 
-        public ClubDto(Club dbo)
+        public ClubDto(Club dbo, int userId)
         {
             Id = dbo.Id;
             Name = dbo.Name;
             Description = dbo.Description;
+            IsRequestToJoin = dbo.IsRequestToJoin;
             GroupId = dbo.GroupId;
             GroupName = dbo.Group?.Name ?? "";
             MemberCount = dbo.MM_User_Club.Count(m => m.IsAccepted);
-            IsAccepted = false; // TODO
+            IsAccepted = dbo.MM_User_Club.Any(m => m.UserId == userId && m.IsAccepted);
+            HasRequested = dbo.MM_User_Club.Any(m => m.UserId == userId && !m.IsAccepted);
+            IsAllowedToJoin = !IsAccepted && !(dbo.Group?.Clubs.SelectMany(m => m.MM_User_Club).Any(m => m.UserId == userId && m.ClubId != Id) ?? false);
         }
     }
 }
