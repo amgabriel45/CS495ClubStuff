8b6c9cedb5d3eb6ac7c0dd49d74fc60fbe772f05
mlivingston <mdlivingston@crimson.ua.edu>
2018-04-18 10:33:37 -0500

    Added Join Club Functionality Clean Up Some code

---
+++ CrimsonClubs/CrimsonClubs/Controllers/Site/JoinController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CrimsonClubs.Controllers.Site
+{
+    public class JoinController : CCSiteController
+    {
+        [HttpGet, Route("join")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
