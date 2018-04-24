301b2eed8155eeacc90e1ee70f120de79f9970a5
mlivingston <mdlivingston@crimson.ua.edu>
2018-04-24 14:50:33 -0500

    Code Clean Up on Web App

---
+++ CrimsonClubs/CrimsonClubs/Content/site/ajaxHelper.js
@@ -0,0 +1,30 @@
+﻿// Example object with ajax method
+var CrimsonCalls = (function () {
+    // ctor
+    function self() { }
+
+    // Ajax request method
+    self.getMyClubs = function (params) {
+        $.ajax({
+            dataType:  "json",
+            type:  'GET',
+            contentType:  "application/json",
+            data:  {},
+            async:  true,
+            processData:  true,
+            url:  'api/clubs/',
+            error: function (xhr, textStatus, errorThrown) {
+                params.error(xhr, textStatus, errorThrown);
+            },
+            success: function (data, textStatus, xhr) {
+                params.success(data);
+            }
+        });
+
+    };
+
+
+    // Return object
+    return self;
+
+})($);
